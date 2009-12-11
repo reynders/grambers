8f9bbf2c7908d4d950bf0e0121e669c15c9a27d8
reynders <joonas.reynders@iki.fi>
2009-12-11 15:49:55 +0200

    Added first ui

--- src/main/scala/grambers/Main.scala
+++ src/main/scala/grambers/Main.scala
@@ -1,6 +1,14 @@
 package grambers
 
-object Main {
+import javax.swing._
+import java.awt._
+
+object Main extends JFrame {
+
+    object MyPanel extends JPanel {
+        val myLabel = new JLabel("I'm alive")
+        add(myLabel)
+    }
 
     def addOne(number:Int): Int = {
         return number + 1;
@@ -8,5 +16,9 @@ object Main {
 
     def main(args:Array[String]) {
         println("I'm alive!")
+        add(MyPanel)
+        pack()
+        setSize(new Dimension(400,300));
+        show()
     }
 }
