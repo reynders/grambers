385456ddc4b81981bcf9fefc0c585cfb7b04ee18
reynders <joonas.reynders@iki.fi>
2009-12-18 15:33:04 +0200

    Universe in wrong package

--- src/test/scala/grambers/Universe.scala
+++ src/test/scala/grambers/Universe.scala
@@ -4,4 +4,10 @@ import java.util._
 
 class Universe() {
     val things : List[Thing] = new ArrayList[Thing]
+    
+    def bigBang {
+        things.foreach(thing => {
+            println(thing);
+        }
+    }
 }
