effb2d0012b1a6889daaf010399aaf5c16f467dd
reynders <joonas.reynders@iki.fi>
2010-01-05 16:04:02 +0200

    Slightly nausios collision detection added

--- src/main/scala/grambers/Main.scala
+++ src/main/scala/grambers/Main.scala
@@ -8,16 +8,16 @@ object Main {
       val yellowCircle = new grambers.Circle(10);
       yellowCircle.color = java.awt.Color.yellow
       yellowCircle.speed = 1.0
-      yellowCircle.direction = 90
-      yellowCircle.location = (50, 50)
+      yellowCircle.direction = 135
+      yellowCircle.location = (0, 50)
       yellowCircle.doYourThing = (yellowCircle) => {yellowCircle.turn(0)}
       universe.things += yellowCircle
 
       val redCircle = new grambers.Circle(10);
       redCircle.color = java.awt.Color.red
       redCircle.speed = 1.0
-      redCircle.direction = 270
-      redCircle.location = (120, 50)
+      redCircle.direction = 315
+      redCircle.location = (50, 00)
       redCircle.doYourThing = (redCircle) => {redCircle.turn(0)}
       universe.things += redCircle
       
