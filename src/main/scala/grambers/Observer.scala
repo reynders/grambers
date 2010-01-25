830b16fac0b9ec1d85b7f9dc73645d865f91afe5
reynders <joonas.reynders@iki.fi>
2010-01-25 10:16:24 +0200

    Cleaned up observer

--- src/main/scala/grambers/Observer.scala
+++ src/main/scala/grambers/Observer.scala
@@ -1,5 +1,6 @@
 package grambers
 
+import java.lang.System._
 import javax.swing._
 import java.awt._
 import java.awt.image._
@@ -14,67 +15,76 @@ class Observer (val universe : Universe) {
     var fps = 0
     
     object WindowToWorld extends JFrame {
-        object ViewPanel extends JPanel {
-           override def paint (g : Graphics) {
-                super.paint(g)
-                g match {
+      initGraphics  
+      
+      object ViewPanel extends JPanel {
+        override def paint (g : Graphics) {
+          super.paint(g)
+          g match {
                     case g2: Graphics2D => drawUniverse(g2)
                     case _ => throw new ClassCastException
-                }
-           }
-        }
-
-        def drawUniverse(g2 : Graphics2D) {
-            clearScreenBuffer()
-            sbg.setColor(Color.black);
-            for (thing <- universe.things) {
-              thing.draw(sbg)      
-            }
-
-            g2.drawImage(screenBuffer, 0, 0, null);
-            fps += 1
-        }
-
-        def clearScreenBuffer() {
-            sbg.setColor(Color.green);
-            sbg.fillRect(0, 0, universe.WIDTH, universe.HEIGHT);
-        }
-        
-        def fpsToMs(fps:int) : int = {
-            return (1000 / fps)
-        }
-
-       
-        def start {
-          add(ViewPanel)
-          pack()
-          setSize(new Dimension(universe.WIDTH, universe.HEIGHT));
-          show()
-          repaint()          
-          import java.lang.System._
-          val worldUpdatesPerSecond = 40;
-          val millisecondsBetweenWorldUpdates = 1000 / worldUpdatesPerSecond
-          var nextWorldUpdateTime = currentTimeMillis
-          var measurementStartTime = currentTimeMillis          
-          var worldUpdates = 0
-          
-          while (true) {
-            val now = currentTimeMillis
-            
-            if (now - measurementStartTime > 1000) {
-              println(fps + " FPS, " + worldUpdates + " world updates")
-              fps = 0; worldUpdates = 0; measurementStartTime = now ; 
-            }
-            
-            while (now > nextWorldUpdateTime) {             
-              universe.advanceTime(millisecondsBetweenWorldUpdates)
-              nextWorldUpdateTime += millisecondsBetweenWorldUpdates
-              worldUpdates+=1                               
-            }
-            
-            repaint()
           }
         }
+      }
+
+      def drawUniverse(g2 : Graphics2D) {
+        clearScreenBuffer()
+        sbg.setColor(Color.black);
+        for (thing <- universe.things) {
+          thing.draw(sbg)      
+        }
+
+        g2.drawImage(screenBuffer, 0, 0, null);
+        fps += 1
+      }
+
+      def clearScreenBuffer() {
+        sbg.setColor(Color.green);
+        sbg.fillRect(0, 0, universe.WIDTH, universe.HEIGHT);
+      }
+        
+      //def fpsToMs(fps:int) : int = {
+        //return (1000 / fps)
+      //}
+
+      def initGraphics {
+        add(ViewPanel)
+        pack()
+        setSize(new Dimension(universe.WIDTH, universe.HEIGHT));
+        show()
+        repaint()          
+      }
+       
+      def start {          
+        val worldUpdatesPerSecond = 40;
+        val millisecondsBetweenWorldUpdates = 1000 / worldUpdatesPerSecond
+        var nextWorldUpdateTime = currentTimeMillis
+                  
+        while (true) {
+          val now = currentTimeMillis
+
+          showStatistics            
+            
+          while (now > nextWorldUpdateTime) {             
+            universe.advanceTime(millisecondsBetweenWorldUpdates)
+            nextWorldUpdateTime += millisecondsBetweenWorldUpdates
+            worldUpdates+=1                               
+          }
+            
+          repaint()
+        }
+      }
+        
+      var measurementStartTime = currentTimeMillis      
+      val measurementSamplePeriodMs = 5000
+      var worldUpdates = 0
+        
+      def showStatistics {
+        if (currentTimeMillis - measurementStartTime > 5000) {
+          println((fps/(measurementSamplePeriodMs/1000)) + " FPS, " + (worldUpdates/(measurementSamplePeriodMs/1000)) + " world updates")
+          fps = 0; worldUpdates = 0; measurementStartTime = currentTimeMillis ; 
+        }
+      }
     }
     
     def observe() {
