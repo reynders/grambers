fe751a4d106047589d7d37f5d3b406e3363b90b1
reynders <joonas.reynders@iki.fi>
2010-04-12 22:35:15 +0300

    Replaced nano timing with ms timing for performance reasons

--- src/main/scala/grambers/Config.scala
+++ src/main/scala/grambers/Config.scala
@@ -21,5 +21,5 @@ object Config {
               (worldUpdates/(measurementSamplePeriodMs/1000)) + " world updates")       
   }
   
-  def currentTimeMillis : Long = System.nanoTime / 1000000
+  def currentTimeMillis : Long = System.currentTimeMillis //System.nanoTime / 1000000
 }
