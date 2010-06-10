249171bfb9f10d88466bd14ef532a4631e41d50b
unknown <Joonas@.(none)>
2010-06-10 08:59:00 +0300

    Removed redundant test cases

--- src/test/scala/grambers/MapTest.scala
+++ src/test/scala/grambers/MapTest.scala
@@ -13,38 +13,8 @@ class MapTest extends TestCase {
     assertEquals((0, 1), map.worldPointToTileIndex(Point(map.tileW/2, map.tileH)))
     assertEquals((1, 0), map.worldPointToTileIndex(Point(map.tileW, map.tileH/2)))
   }
-  
-  def testBase64Decode {
-      val byteArray : Array[Byte] = Base64.decode("dGVzdA==") 
-      assertEquals("test", new String(byteArray))
-    }
-    
-    def testGZipDecode {
-      val encoded = Base64.encodeBytes("testgzip".getBytes(), Base64.GZIP);     
-      assertTrue(encoded != null)
-      assertTrue(!encoded.equals("testgzip"))
-      assertEquals("testgzip", new String(Base64.decode(encoded)))
-    }
-    
-    def testTiledDecode {
-      val byteArray : Array[Byte] = Base64.decode("H4sIAAAAAAAAC2NkYGBggmJGKMbFZwZiAJN2z9cwAAAA")
-      for (i <- 0 until byteArray.size) {
-        if (i%4 == 0)
-          println(i + ":" + BigInt(byteArray.slice(i, i+4).reverse))
-      }
-      assertTrue(1==1)
-    }
-    
-    def testFileLoad = {
-      import scala.xml._
-      //val file = scala.io.Source.fromFile(new java.io.File("resources/maps/test.tmx")).mkString
-      val xml = Utility.toXML(XML.loadFile("resources/maps/test.tmx"))
-      println("Xml: " + xml)
       
-      assertTrue(xml.startsWith("<map"))
-    }
-    
-    val testMapXml = <map version="1.0" orientation="orthogonal" width="4" height="3" tilewidth="32" tileheight="32">
+  val testMapXml = <map version="1.0" orientation="orthogonal" width="4" height="3" tilewidth="32" tileheight="32">
                     <tileset firstgid="1" name="tileset0" tilewidth="32" tileheight="32" spacing="1">
                       <image source="resources/gfx/testtileset_8x6.png"/>
                     </tileset>
