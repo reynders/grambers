06b2f531eb057b9c0532e04fc97547bc1ed64db8
Joonas Reynders <joonas.reynders@iki.fi>
2012-01-12 10:29:10 +0200

    Updated build files

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+
+# For debugging jagged graphics
+#java -verbose:gc -XX:+PrintCompilation -client -cp $SCALA_HOME/lib/scala-library.jar:./target/classes:lib/base64-2.3.8.jar $@
+
 java -client -cp $SCALA_HOME/lib/scala-library.jar:./target/classes:lib/base64-2.3.8.jar $@
