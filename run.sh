f9d283fee5c291c206db15d3a8b42b11d0a56592
Joonas Reynders <joonas.reynders@iki.fi>
2012-02-28 09:21:13 +0200

    Interpreted only mode

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # For debugging jagged graphics
 #java -verbose:gc -XX:+PrintCompilation -client -cp $SCALA_HOME/lib/scala-library.jar:./target/classes:lib/base64-2.3.8.jar $@
 
-java -client -cp $SCALA_HOME/lib/scala-library.jar:./target/classes:lib/base64-2.3.8.jar $@
+java -client -Xint -cp $SCALA_HOME/lib/scala-library.jar:./target/classes:lib/base64-2.3.8.jar $@
