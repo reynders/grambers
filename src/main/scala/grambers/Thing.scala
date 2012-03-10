89b6c4053fcfda904295f8609a2e9f89393fc4ea
Joonas Reynders <joonas.reynders@iki.fi>
2012-03-10 23:03:28 +0200

    Clean up Thing from dead code

--- src/main/scala/grambers/Thing.scala
+++ src/main/scala/grambers/Thing.scala
@@ -62,18 +62,6 @@ abstract class MovingThing(location : Point) extends Thing {
   }
 }
 
-/*
-class Wall(val start:Point, val end:Point) extends Thing(Point(end.x - start.x, end.y-start.y), 0, 0) extends StaticThing {
-  def shape : Shape = {
-    Line(start, end)
-  }
-  def draw(g2 : Graphics2D, position : Point ) = {}
-  
-  override def toString : String = {
-    return "Wall(" + start + "," + end + ")"
-  }
-}*/
-
 class CircleMovingThing(var c : Point, val radius : Double) extends MovingThing(c) {
   
   var color = java.awt.Color.yellow
@@ -187,10 +175,6 @@ class RectangleStaticThing(val c : Point, val w : Int, val h : Int) extends Stat
   override def drawDebugShapes(g2 : Graphics2D, position : Point) = {}
 }
 
-/*
-      extends PolygonStaticThing(lup, Array((lup.x.toInt, lup.y.toInt), (lup.x.toInt + w, lup.y.toInt),
-                                            (lup.x.toInt + w, lup.y.toInt + h), (lup.x.toInt, lup.y.toInt + h)).toList)*/
-
 import java.awt.image._
 import java.io._
 
@@ -242,101 +226,3 @@ class PolygonMovingThing(var c : Point, val sprite : Sprite) extends MovingThing
     return "PolygonMovingThing: " + super.toString
   }
 }
-
-/*
-object RoundThing {
-  def apply(x : Double, y : Double, r : Double, mass : Double, color: Color, speed : Double, direction : Double) : RoundThing = {
-    val ball = new RoundThing(Point(x, y), r)
-    ball.mass = mass
-    ball.color = color
-    ball.speed = speed
-    ball.direction = direction
-    return ball
-  }
-}
-
-class Box(c : Point, w : Double, h : Double) extends Thing(c, w, h) with StaticThing {
-  
-  def this(w : Double, h : Double) = this(Point(0, 0), w, h)
-  
-  var color = java.awt.Color.black
-
-  def shape : Shape = {
-    new Rectangle((center.x-(w/2), center.y-(h/2)), (center.x+(w/2), center.y+(h/2)))
-  }
-  
-  val image = new Rectangle2D.Double(center.x - w/2, center.y-h/2, w, h)
-  
-  def draw(g2: Graphics2D, position : Point) {    
-    image.setFrame((position.x - w/2), (position.y-h/2), w, h)
-    val originalPaintColor = g2.getPaint()
-    g2.setPaint(color)
-    g2.fill(image)
-    g2.setPaint(java.awt.Color.green)
-    g2.drawRect(position.x.toInt, position.y.toInt, 2, 2)
-    g2.setPaint(originalPaintColor)
-  }
-  
-  override def toString : String = {
-    return "Box" + super.toString + "," + w + "w," + h + "h"
-  }
-}
-
-object Box {
-  def apply(x : Double, y : Double, w : Double, h : Double, color : Color) : Box = {
-    val box = new Box(new Point(x, y), w, h)
-    box.color = color
-    return box
-  }
-}
-
-import java.awt.image._
-import java.io._
-
-class ImageRoundThing(var cntr : Point, val rad:Double, fileName : String) extends RoundThing(cntr, rad) with MovingThing {
-  
-  //def this(radius : Double) = this(new Point(0, 0), radius)
-  val ROTATED_IMAGE_COUNT = 36   
-  val img : BufferedImage = javax.imageio.ImageIO.read(new File(fileName)).asInstanceOf[BufferedImage]
-  val rotatedImage = createRotatedImages(img)
-  
-  import scala.collection.mutable._
-  def createRotatedImages(image : BufferedImage) : Buffer[Image] = {
-    val images = new ArrayBuffer[Image]()
-
-    for(i <- 0 until ROTATED_IMAGE_COUNT) {
-      //val rotatedImage = image.getGraphics().asInstanceOf[Graphics2D].getDeviceConfiguration().createCompatibleImage(image.getWidth, image.getHeight, Transparency.TRANSLUCENT)      
-      val diameter = Math.max(image.getWidth, image.getHeight)
-      val rotatedImage = new BufferedImage(diameter, diameter, Config.imageType)
-      val at = new AffineTransform()
-      at.rotate(toRadians(i*(360/ROTATED_IMAGE_COUNT)), diameter/2, diameter/2)
-      at.translate(Math.abs(diameter-image.getWidth)/2, Math.abs(diameter-image.getHeight)/2)
-      val g2d = rotatedImage.createGraphics.asInstanceOf[Graphics2D]      
-      g2d.drawImage(image, new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR), 0, 0)
-      images += rotatedImage
-    }
-    
-    return images
-  }
-    
-  def getRotatedImageNumberBasedOnDirection : Int = direction.toInt / (360 / ROTATED_IMAGE_COUNT)
-  
-  override def draw(g2: Graphics2D, position : Point) {
-    g2.drawImage(rotatedImage(getRotatedImageNumberBasedOnDirection), (position.x - w/2).toInt, (position.y-h/2).toInt, null)
-  }
-
-  override def toString : String = {
-    return super.toString + ":" + radius + "r"
-  }
-}
-
-object ImageRoundThing {
-  def apply(x : Double, y : Double, r : Double, mass : Double, fileName : String, speed : Double, direction : Double) : ImageRoundThing = {
-    val ball = new ImageRoundThing(Point(x, y), r, fileName)
-    ball.mass = mass
-    ball.speed = speed
-    ball.direction = direction
-    return ball
-  }
-}
-*/
