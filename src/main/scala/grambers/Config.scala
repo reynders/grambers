9b0a49e64631fe6abcf9292ca0cdb4737fb999e0
Joonas Reynders <joonas.reynders@iki.fi>
2012-05-02 16:13:07 +0300

    Fixed mass body and forces for the demo ship

--- src/main/scala/grambers/Config.scala
+++ src/main/scala/grambers/Config.scala
@@ -6,9 +6,9 @@ object Config {
   val worldUpdateDt : Float = 1.0f/worldUpdatesPerSecond
   val velocityIterations = 10
   val positionIterations = 10
-  
+
   // Gfx settings
-  val imageType = java.awt.image.BufferedImage.TYPE_INT_ARGB 
+  val imageType = java.awt.image.BufferedImage.TYPE_INT_ARGB
   val ROTATED_IMAGE_COUNT = 36
   val limitFps = false
   val fpsLimit = 5
@@ -18,26 +18,26 @@ object Config {
   var fps = 0
   var worldUpdates = 0
   var measurementStartTime = currentTimeMillis
-  
+
   // Debug
   var debugOn : Boolean = false
-  val debugDrawShapes : Boolean = false
+  val debugDrawShapes : Boolean = true
   val debugDrawShapesColor : java.awt.Color = java.awt.Color.RED
-  
+
   def measurementSamplePeriodMs : Long = {
     currentTimeMillis - measurementStartTime
   }
-  
+
   def resetAnimationMeasurements {
     fps = 0; worldUpdates = 0; measurementStartTime = currentTimeMillis ;
   }
-  
+
   def showAnimationStatistics {
     if (measurementSamplePeriodMs > 0)
-      println((fps/(measurementSamplePeriodMs/1000)) + " FPS, " + 
-              (worldUpdates/(measurementSamplePeriodMs/1000)) + " world updates")       
+      println((fps/(measurementSamplePeriodMs/1000)) + " FPS, " +
+              (worldUpdates/(measurementSamplePeriodMs/1000)) + " world updates")
   }
-  
-  def currentTimeMillis : Long = // System.currentTimeMillis 
+
+  def currentTimeMillis : Long = // System.currentTimeMillis
                                  System.nanoTime / 1000000
 }
